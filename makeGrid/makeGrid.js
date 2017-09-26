5b7c60715a821ed33da386ec893e9109b2880df8
Jakub Hrubý <mail@jakubhruby.cz>
2017-09-26 11:25:39 +0200

    makeGrid - heights computing fix

--- makeGrid/makeGrid.js
+++ makeGrid/makeGrid.js
@@ -1,11 +1,10 @@
 function makeGrid(wrapperEl) {
 	var
-		i, highestCol, post,
+		i, highestCol, lowestCol, post,
 		wrapperElWidth = wrapperEl.clientWidth,
 		posts = wrapperEl.children,
 		col_heights = [],
 		order = 1,
-		COMPARE_OFFSET = 20,
 		COL_COUNT = getColCount();
 
 	wrapperEl.style.display = 'flex';
@@ -26,20 +25,13 @@ function makeGrid(wrapperEl) {
 
 	for (i = 0; i < posts.length; i++) {
 		post = posts[i];
+		lowestCol = Math.min.apply(Math, col_heights);
+		order = col_heights.indexOf(lowestCol) + 1;
 		post.style.order = order;
 		post.style.width = Math.floor(wrapperElWidth / COL_COUNT) + 'px';
-
 		col_heights[order - 1] += parseFloat(post.clientHeight);
-		highestCol = Math.max.apply(Math, col_heights);
-
-		if (col_heights[order - 1] >= highestCol - COMPARE_OFFSET) {
-			order++;
-
-			if (order > COL_COUNT) {
-				order = 1;
-			}
-		}
 	}
 
+	highestCol = Math.max.apply(Math, col_heights);
 	wrapperEl.style.height = highestCol + 'px';
 }
